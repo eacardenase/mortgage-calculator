2795077bb1aef02d50ef1f71ef3f757e83ba0dd4
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-06-13 21:32:06 -0500

    Version 2.0

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,34 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
+        System.out.print("Principal ($1K - $1M): ");
         int principal = scanner.nextInt();
-        System.out.print("Annual Interest Rate: ");
-        double annualInterest = scanner.nextDouble() / PERCENT;
-        System.out.print("Period (Years): ");
-        int numberOfPayments = scanner.nextInt() * MONTHS_IN_YEAR;
+        while (principal < 1_000 || principal > 1_000_000) {
+            System.out.println("Enter a number between 1,000 and 1,000,000");
+            principal = scanner.nextInt();
+        }
 
-        double monthlyInterest = annualInterest / MONTHS_IN_YEAR;
+        System.out.print("Annual Interest Rate: ");
+        double annualInterest = scanner.nextDouble();
+        while (annualInterest <= 0 || annualInterest > 30) {
+            System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+            annualInterest = scanner.nextDouble();
+        }
+
+        System.out.print("Period (Years): ");
+        int years = scanner.nextInt();
+
+        while (years < 1 || years > 30) {
+            System.out.println("Enter a value between 1 and 30");
+            years = scanner.nextInt();
+        }
+
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
         double mortgage = principal
-                * ((monthlyInterest * Math.pow((double)(1 + monthlyInterest), (double) numberOfPayments))
-                / (Math.pow((double)(1 + monthlyInterest), (double) numberOfPayments) - 1));
+                * ((monthlyInterest * Math.pow((1 + monthlyInterest), numberOfPayments))
+                / (Math.pow((1 + monthlyInterest), numberOfPayments) - 1));
 
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortgage: " + mortgageFormatted);
